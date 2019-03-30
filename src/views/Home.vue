f9e2d6b9a8ebfe7c6faf8952541d7e15ea405fc1
tejapeddiboyina <tejapeddiboyina@users.noreply.github.com>
2019-03-30 23:46:49 +0530

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,81 @@
+<!--<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>-->
+<template>
+  <div id="app">
+
+
+    <div @click="setFalse()">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <button @click="getName" class="btn btn-primary" style="margin:20px">click to enter your name</button>
+      <span>welcome {{firstName}}</span><br><br>
+    </div>
+    
+
+    <div>
+      <router-link to="/AddBook" class="btn btn-primary" style="margin:20px" @getAddedBook="pushAddedBook">AddBook</router-link>
+      <router-link :to="{name:'ReadBook', params:{books}}" class="btn btn-info" style="margin:20px">ReadBook</router-link>
+    </div>
+    <router-view />
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return{
+      message:'Welcome to Your Vue.js App',
+      firstName: '',
+      books:[{
+        name:'vueJs',
+        description:'vuejs for beginner',
+        image: require('../assets/logo.png'),
+        url:'https://vuejs.org/'
+        },
+        {
+          name:'angularjs',
+          description:'angularjs for beginner',
+          image: require('../assets/ang.png'),
+          url:'https://angularjs.org/'
+      }],
+    }
+  },
+  methods: {
+    getName() {
+      this.firstName = prompt("enter your name");
+    },
+    pushAddedBook(book) {
+      this.books.push(book)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ src/views/ReadBook.vue
@@ -0,0 +1,45 @@
+<template>
+    <div> 
+        <h1 class="alert alert-info">Read a Book</h1><br><br>
+        <div class="container">
+            <div v-if="readBooks.length > 0" class="row">
+                <div v-for="book in readBooks" :key="book.name" class="col-sm-4">
+                    <img alt="logo" :src="book.image" style="width:200px;height:200px">
+                    <a :href="book.url" :title="book.description">{{book.name}}</a>
+                </div>
+            </div>
+            <div v-else>
+                no books
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'ReadBook',
+    props: ['books'],
+    data() {
+        return {
+            readBooks:[{
+            name:'react',
+            description:'react for beginner',
+            image: require('../assets/react.png'),
+            url:'https://reactjs.org/'
+            }],
+        }
+    },
+    async mounted() {
+        this.books.forEach(book => {
+           this.readBooks.push(book) 
+        });
+    }
+}
+
+</script>
+
+<style>
+
+
+</style>
